af776b0b3655e8bd35a752ca07eeac779bd17331
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-03-06 22:51:47 +0600

    The Validation is done along with firebase authentication

--- app/src/main/java/com/example/habithive/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/habithive/activities/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.habithive.activities;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.habithive.R;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,6 +20,9 @@ public class RegistrationActivity extends AppCompatActivity {
     private TextInputEditText usernameEditText;
     private TextInputEditText emailEditText;
     private TextInputEditText passwordEditText;
+    private TextInputLayout userNameLayout;
+    private TextInputLayout emailLayout;
+    private TextInputLayout passwordLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,10 +31,13 @@ public class RegistrationActivity extends AppCompatActivity {
 
 //        Initialize Firebase Auth
         auth = FirebaseAuth.getInstance();
-
+//        Binding with component
         usernameEditText = findViewById(R.id.UserNameEditText);
         emailEditText = findViewById(R.id.EmailEditText);
         passwordEditText = findViewById(R.id.PasswordEditText);
+        userNameLayout = findViewById(R.id.UserNameLayout);
+        emailLayout = findViewById(R.id.EmailLayout);
+        passwordLayout = findViewById(R.id.PasswordLayout);
 
         Button RegisterButton = findViewById(R.id.registerButton);
 
@@ -53,7 +61,39 @@ public class RegistrationActivity extends AppCompatActivity {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
 
+//        Clear Previous Errors
+        userNameLayout.setError(null);
+        emailLayout.setError(null);
+        passwordLayout.setError(null);
+
         //        I will validation here later
+        boolean haserrors = false;
+        if(username.isEmpty())
+        {
+            userNameLayout.setError("Required");
+            haserrors = true;
+        }
+        if(email.isEmpty())
+        {
+            emailLayout.setError("Required");
+            haserrors = true;
+        }
+        if(password.isEmpty())
+        {
+            passwordLayout.setError("Required");
+            haserrors = true;
+        }
+
+        if(haserrors)
+        {
+            return;
+        }
+
+        if(password.length() < 6)
+        {
+            passwordLayout.setError("Password must be at least of 6 characters");
+        }
+
 
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this,task -> {
             if(task.isSuccessful())
@@ -63,7 +103,8 @@ public class RegistrationActivity extends AppCompatActivity {
             }
             else
             {
-                Toast.makeText(RegistrationActivity.this,"Error: " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+//                Log.d("Error",task.getException().getMessage());
+//                Toast.makeText(RegistrationActivity.this,"An Unknown Error.Please Try Again",Toast.LENGTH_SHORT).show();
             }
         });
 
