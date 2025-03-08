44a31118e5832a93d9f068ea1ec6189005b5cdbf
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-03-08 15:15:19 +0600

    Functinality related image picking is enabled

--- app/src/main/java/com/example/habithive/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/habithive/activities/RegistrationActivity.java
@@ -1,9 +1,13 @@
 package com.example.habithive.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +27,8 @@ public class RegistrationActivity extends AppCompatActivity {
     private TextInputLayout userNameLayout;
     private TextInputLayout emailLayout;
     private TextInputLayout passwordLayout;
+    private ImageView addImage;
+    private ImageView imagePlaceHolder;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -32,84 +38,35 @@ public class RegistrationActivity extends AppCompatActivity {
 //        Initialize Firebase Auth
 //        auth = FirebaseAuth.getInstance();
 ////        Binding with component
-//        usernameEditText = findViewById(R.id.UserNameEditText);
-//        emailEditText = findViewById(R.id.EmailEditText);
-//        passwordEditText = findViewById(R.id.PasswordEditText);
-//        userNameLayout = findViewById(R.id.UserNameLayout);
-//        emailLayout = findViewById(R.id.EmailLayout);
-//        passwordLayout = findViewById(R.id.PasswordLayout);
-//
-//        Button RegisterButton = findViewById(R.id.registerButton);
-//
-//
-////        Handle Sign Up Button Click
-//
-//        RegisterButton.setOnClickListener(new View.OnClickListener()
-//        {
-//            @Override
-//            public void onClick(View v)
-//            {
-//                registerUser();
-//            }
-//        });
-//
-//
-//    }
-//    private void registerUser()
-//    {
-//        String username = usernameEditText.getText().toString().trim();
-//        String email = emailEditText.getText().toString().trim();
-//        String password = passwordEditText.getText().toString().trim();
-//
-////        Clear Previous Errors
-//        userNameLayout.setError(null);
-//        emailLayout.setError(null);
-//        passwordLayout.setError(null);
-//
-//        //        I will validation here later
-//        boolean haserrors = false;
-//        if(username.isEmpty())
-//        {
-//            userNameLayout.setError("Required");
-//            haserrors = true;
-//        }
-//        if(email.isEmpty())
-//        {
-//            emailLayout.setError("Required");
-//            haserrors = true;
-//        }
-//        if(password.isEmpty())
-//        {
-//            passwordLayout.setError("Required");
-//            haserrors = true;
-//        }
-//
-//        if(haserrors)
-//        {
-//            return;
-//        }
-//
-//        if(password.length() < 6)
-//        {
-//            passwordLayout.setError("Password must be at least of 6 characters");
-//        }
-//
-//
-//        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this,task -> {
-//            if(task.isSuccessful())
-//            {
-//                FirebaseUser user = auth.getCurrentUser();
-//                Toast.makeText(RegistrationActivity.this,"Registration Successfull",Toast.LENGTH_SHORT).show();
-//            }
-//            else
-//            {
-////                Log.d("Error",task.getException().getMessage());
-////                Toast.makeText(RegistrationActivity.this,"An Unknown Error.Please Try Again",Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
+          addImage = findViewById(R.id.AddImage);
+          imagePlaceHolder = findViewById(R.id.ImagePlaceHolder);
 
-
+//            Handling the click on the "+" sign
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImagePicker();
+            }
+        });
 
     }
+//    Method to open the image picker
+    private void openImagePicker()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,1);
+    }
+//    Handle the result of the image picker
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data!=null)
+        {
+            Uri imageUri = data.getData();
+            imagePlaceHolder.setImageURI(imageUri);
+        }
+
+    }
+
 }
